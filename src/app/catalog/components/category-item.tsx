6ab32d6f7a9c638d395fd3be4cedaa3bff6a9233
Gabriel Sakoda <otanigabriel0@gmail.com>
2023-10-25 21:22:51 -0300

    feat: add page to show category products

--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -1,4 +1,5 @@
 import { Category } from "@prisma/client";
+import { Link } from "lucide-react";
 import Image from "next/image"
 
 interface CategoryItemProps {
@@ -6,25 +7,28 @@ interface CategoryItemProps {
 }
 
 const CategoryItem = ({category }: CategoryItemProps) => {
-    return ( <div className="flex flex-col">
-        <div className="bg-category-item-gradient w-full h-[150px] flex items-center justify-center rounded-tl-lg rounded-tr-lg">
-            <Image
-                src={category.imageUrl}
-                alt={category.name}
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="h-auto max-h-[70%] w-auto max-w[80%]"
-                style={{
-                    objectFit: "contain",
-                }}
-            />
-        </div>
+    return ( 
+        <Link href={`/category/${category.slug}`}>
+            <div className="flex flex-col">
+                <div className="bg-category-item-gradient w-full h-[150px] flex items-center justify-center rounded-tl-lg rounded-tr-lg">
+                    <Image
+                        src={category.imageUrl}
+                        alt={category.name}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-auto max-h-[70%] w-auto max-w-[80%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                    />
+                </div>
 
-        <div className="bg-accent py-3 rouned-br-lg roundend-bl-lg">
-            <p className="text-center text-sm font-semibold">{category.name}</p>
-        </div>
-    </div>
+                <div className="bg-accent py-3 rouned-br-lg roundend-bl-lg">
+                    <p className="text-center text-sm font-semibold">{category.name}</p>
+                </div>
+            </div>
+        </Link>
 
     );
 }
