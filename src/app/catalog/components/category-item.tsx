4e8270b0a4631ccdcf662d6bb9460594d8350847
Gabriel Sakoda <otanigabriel0@gmail.com>
2023-10-25 21:39:31 -0300

    fix: fixing the category item page

--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -1,36 +1,33 @@
 import { Category } from "@prisma/client";
-import { Link } from "lucide-react";
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
-    category: Category
+  category: Category;
 }
 
-const CategoryItem = ({category }: CategoryItemProps) => {
-    return ( 
-        <Link href={`/category/${category.slug}`}>
-            <div className="flex flex-col">
-                <div className="bg-category-item-gradient w-full h-[150px] flex items-center justify-center rounded-tl-lg rounded-tr-lg">
-                    <Image
-                        src={category.imageUrl}
-                        alt={category.name}
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="h-auto max-h-[70%] w-auto max-w-[80%]"
-                        style={{
-                            objectFit: "contain",
-                        }}
-                    />
-                </div>
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <Link href={`/category/${category.slug}`}>
+      <div className="flex flex-col">
+        <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
+          {/* IMAGEM */}
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          />
+        </div>
 
-                <div className="bg-accent py-3 rouned-br-lg roundend-bl-lg">
-                    <p className="text-center text-sm font-semibold">{category.name}</p>
-                </div>
-            </div>
-        </Link>
+        <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
+          <p className="text-center text-sm font-semibold">{category.name}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-    );
-}
-
-export default CategoryItem
+export default CategoryItem;
