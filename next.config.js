eccd299dfb5cd2f50f71e3468410a600aa856088
Gabriel Sakoda <otanigabriel0@gmail.com>
2023-11-04 21:21:58 -0300

    feat: add stripe checkout to cart

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
     images: {
         domains: ["fsw-store.s3.sa-east-1.amazonaws.com"] // permite que imagens desse domínio sejam carregadas
-
     },
+    experimental: {
+        serverActions: true,
+    }, 
 };
 
 module.exports = nextConfig
