e9195339aeee4922cd15f2a1e678fb35740714e8
Gabriel Sakoda <otanigabriel0@gmail.com>
2023-11-04 23:22:40 -0300

    fix: fixing the next config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
     images: {
         domains: ["fsw-store.s3.sa-east-1.amazonaws.com"] // permite que imagens desse domínio sejam carregadas
     },
-    experimental: {
-        serverActions: true,
-    }, 
 };
 
 module.exports = nextConfig
